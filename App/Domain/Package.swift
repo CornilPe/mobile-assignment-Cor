2a87071bc6b3a245cd9b89d4b83a9e81af2b2868
Crit <corn21p@gmail.com>
2023-04-22 13:56:57 +0400

    Changed structure of code template.

--- App/Domain/Package.swift
+++ App/Domain/Package.swift
@@ -6,14 +6,11 @@ import PackageDescription
 let package = Package(
   name: "Domain",
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "RocketsClient",
       targets: ["RocketsClient"]),
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
   ],
   targets: [
     .target(
---
+++ App/RocketApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct RocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
+
